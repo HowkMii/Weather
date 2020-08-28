092c2f269d30109a5bb2e081b687a104d5fa62fb
unknown <abdelhakim@univ-tiaret.dz>
2020-08-28 21:07:58 +0100

    create react weather application

---
+++ src/App_component/weather.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Weather = () => {
+    return(
+        <div className="container">
+            <div className="cards">
+                <h1>Tiaret</h1>
+                <h5 className="py-4">
+                    <i className="wi wi-day-sunny"></i>
+
+                </h5>
+            </div>
+
+        </div>
+
+    );
+  
+};
+export default Weather;
